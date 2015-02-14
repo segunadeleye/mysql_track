53b6d90ada4d95baf959e432032c37f0f4617c87
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-02-14 11:45:40 +0100

    Exercise 6: Email Campaign Data.

--- exercise_6.sql
+++ exercise_6.sql
@@ -1,11 +1,55 @@
-create table Emails (
-  email varchar(40) not null,
-  phone varchar(10) not null,
-  city varchar(15) not null
+CREATE TABLE Emails (
+  email VARCHAR(40) NOT NULL,
+  phone VARCHAR(10) NOT NULL,
+  city VARCHAR(15) NOT null
 );
 
-load data local infile 'email_subscribers.txt'
-into table Emails
-fields terminated by ','
-enclosed by '"'
-lines terminated by '\n';
+LOAD DATA LOCAL INFILE 'email_subscribers.txt'
+INTO TABLE EMAILS
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n';
+
+--1--
+SELECT DISTINCT city FROM Emails;
+
+--2--
+SELECT city, COUNT(city) AS `Number of Response`
+FROM Emails
+GROUP BY city;
+
+--3--
+SELECT city, COUNT(city) AS `Number of Response`
+FROM Emails
+GROUP BY city
+HAVING COUNT(city) = (
+  SELECT MAX(`Number of Response`)
+  FROM (
+    SELECT COUNT(city) AS `Number of Response`
+    FROM Emails
+    GROUP BY city
+  ) Response_Count
+);
+
+--4--
+SELECT substring_index(email, '@', -1) AS `Email Domain`
+FROM Emails
+GROUP BY `Email Domain`;
+
+--5--
+SELECT email_domain, COUNT(email_domain) AS amount
+FROM (
+  SELECT substring_index(email, '@', -1) AS email_domain
+  FROM Emails
+) AS Domains
+GROUP BY email_domain 
+HAVING amount = (
+  SELECT MAX(amount)
+  FROM (
+    SELECT COUNT(email_domain) AS amount
+    FROM (
+      SELECT substring_index(email, '@', -1) AS email_domain
+      FROM Emails
+    ) AS Domains
+    GROUP BY email_domain
+  ) AS Domain_Count
+);
