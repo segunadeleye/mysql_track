72eddb7a60e5f8c146e886211458bad594dd8d4b
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-02-18 15:34:00 +0100

    Rename table Emails to SUbscribers.

--- exercise_6.sql
+++ exercise_6.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Emails (
+CREATE TABLE Subscribers (
   email VARCHAR(40) NOT NULL,
   phone VARCHAR(10) NOT NULL,
   city VARCHAR(15) NOT NULL,
@@ -6,41 +6,41 @@ CREATE TABLE Emails (
 );
 
 LOAD DATA LOCAL INFILE 'email_subscribers.txt'
-INTO TABLE EMAILS
+INTO TABLE Subscribers
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
 --1--
-SELECT DISTINCT city FROM Emails;
+SELECT DISTINCT city FROM Subscribers;
 
 --2--
 SELECT city, COUNT(city) AS `Number of Response`
-FROM Emails
+FROM Subscribers
 GROUP BY city;
 
 --3--
 SELECT city, COUNT(city) AS `Number of Response`
-FROM Emails
+FROM Subscribers
 GROUP BY city
 HAVING COUNT(city) = (
   SELECT MAX(`Number of Response`)
   FROM (
     SELECT COUNT(city) AS `Number of Response`
-    FROM Emails
+    FROM Subscribers
     GROUP BY city
   ) Response_Count
 );
 
 --4--
 SELECT substring_index(email, '@', -1) AS `Email Domain`
-FROM Emails
+FROM Subscribers
 GROUP BY `Email Domain`;
 
 --5--
 SELECT email_domain, COUNT(email_domain) AS amount
 FROM (
   SELECT substring_index(email, '@', -1) AS email_domain
-  FROM Emails
+  FROM Subscribers
 ) AS Domains
 GROUP BY email_domain 
 HAVING amount = (
@@ -49,7 +49,7 @@ HAVING amount = (
     SELECT COUNT(email_domain) AS amount
     FROM (
       SELECT substring_index(email, '@', -1) AS email_domain
-      FROM Emails
+      FROM Subscribers
     ) AS Domains
     GROUP BY email_domain
   ) AS Domain_Count
